225e8972f640b9c72144c925f075d54f23a26ff2
GenThrive <genthrive@ecorise.org>
2025-01-27 12:33:09 -0600

    added the project screens in the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import Siderbar from "./components/Sidebar";
 
 function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
       <Siderbar />
+      {/* <NewProject /> */}
+      <NoProjectSelected />
     </main>
   );
 }
